5808a91f1491f01286d83d66c5420dcc8c10a29a
chalapathi <harijanachalapathi7@gmail.com>
2026-05-11 21:39:19 +0530

    added file

--- src/main/java/Problems/ReadTextFiles.java
+++ src/main/java/Problems/ReadTextFiles.java
@@ -1,9 +1,9 @@
 package Problems;
 
-public class FindKthSmallestAndLargest {
+public class ReadTextFiles {
 
 	public static void main(String[] args) {
-		
+		// TODO Auto-generated method stub
 
 	}
 
